d6c32d21052adb43e027afd104ea1fa9d67d9051
Tien <tieeeeen1994@gmail.com>
2025-04-24 10:04:13 +0800

    Merge sandbox var adding and registering into one function

--- media/lua/server/TaskBoard_SandboxVars.lua
+++ media/lua/server/TaskBoard_SandboxVars.lua
@@ -1,18 +1,12 @@
 require("Sandbox/SandboxOptions")
 
-local function addTaskBoardSandboxVars()
+local function addAndRegisterTaskBoardSandboxVars()
     SandboxVars.TaskBoard = SandboxVars.TaskBoard or {}
     SandboxVars.TaskBoard.UseInGameTime = SandboxVars.TaskBoard.UseInGameTime or false -- Boolean toggle
-end
 
-Events.OnGameBoot.Add(addTaskBoardSandboxVars)
-
--- Register the sandbox option
-local function registerTaskBoardSandboxOptions()
+    -- Access the sandbox variable everywhere: local useInGameTime = SandboxVars.TaskBoard.UseInGameTime
     local taskBoardOptions = SandboxOptions.getOptionByName("TaskBoard") or SandboxOptions.addCategory("TaskBoard")
     taskBoardOptions:addOption("UseInGameTime", "Use In-Game Time", false, "By default, time used is real-world time.")
 end
 
-Events.OnGameBoot.Add(registerTaskBoardSandboxOptions)
-
--- Access the sandbox variable everywhere: local useInGameTime = SandboxVars.TaskBoard.UseInGameTime
+Events.OnGameBoot.Add(addAndRegisterTaskBoardSandboxVars)
