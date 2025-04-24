7b77b34c43d5544ad59c7b98bf5f60ae6d748b35
Tien <tieeeeen1994@gmail.com>
2025-04-24 10:06:52 +0800

    improve tooltip for user friendliness

--- media/lua/server/TaskBoard_SandboxVars.lua
+++ media/lua/server/TaskBoard_SandboxVars.lua
@@ -6,7 +6,7 @@ local function addAndRegisterTaskBoardSandboxVars()
 
     -- Access the sandbox variable everywhere: local useInGameTime = SandboxVars.TaskBoard.UseInGameTime
     local taskBoardOptions = SandboxOptions.getOptionByName("TaskBoard") or SandboxOptions.addCategory("TaskBoard")
-    taskBoardOptions:addOption("UseInGameTime", "Use In-Game Time", false, "By default, time used is real-world time.")
+    taskBoardOptions:addOption("UseInGameTime", "Use In-Game Time", false, "Pertains to dates used in the task board, such as creation date of a task. By default, time used is real-world time.")
 end
 
 Events.OnGameBoot.Add(addAndRegisterTaskBoardSandboxVars)
