59c2eb1a0b3fa125dd579b9f4a338a32cbae6c11
Jonorsky Navarrete <jonorskydev@gmail.com>
2025-04-25 22:17:07 +0800

    Supports Single Player via core

--- media/lua/server/TaskBoard_Server.lua
+++ media/lua/server/TaskBoard_Server.lua
@@ -1,42 +1,20 @@
--- Server
 MODDATA_KEY = "KB.KanbanBoard"
-
--- function generateUUID()
---     local db = ModData.getOrCreate(MODDATA_KEY)
-
---     local function padWithZeros(num)
---         return string.format("%010d", num) -- Ensures the number is 10 digits long with leading zeros
---     end
-
---     for i = 1, 5 do
---         local id = padWithZeros(ZombRand(0, 1000000000)) -- Generate a number and pad it
---         if not db[id] then return id end
---     end
-
---     return padWithZeros(ZombRand(0, 1000000000)) -- Fallback in case of collision
--- end
 
 Events.OnClientCommand.Add(function(module, command, player, task)
     if module ~= MODDATA_KEY then return end
 
     local db = ModData.getOrCreate(MODDATA_KEY)
-    -- local transmit = function() ModData.transmit(MODDATA_KEY) end
 
     if command == "CreateTask" then
         TaskBoard_Core.create(task)
-        -- task.id = generateUUID()
-        -- db[task.id] = task
-        -- transmit()
-    -- elseif command == "UpdateTask" and db[task.id] then
+
     elseif command == "UpdateTask" then
         TaskBoard_Core.update(task)
-        -- db[task.id] = task
-        -- transmit()
+
     elseif command == "DeleteTask" then
         TaskBoard_Core.delete(task)
-        -- db[task.id] = nil
-        -- transmit()
-    elseif command == "FetchAllTasks" or command == "ReloadAllTables" then
-        sendServerCommand(player, MODDATA_KEY, command, { tasks = db })
+
+    elseif command == "ReloadAllTables" then
+        TaskBoard_Core.reloadAllTables(player)
     end
 end)
--- media/lua/shared/TaskBoard_Core.lua
+++ media/lua/shared/TaskBoard_Core.lua
@@ -1,115 +1,93 @@
--- Persistency Manager
--- Hook under onGameStart then adapt TaskBoard Core
-    MODDATA_KEY = "KB.KanbanBoard"
+MODDATA_KEY = "KB.KanbanBoard"
 
-    TaskBoard_Core = {}
+TaskBoard_Core = {}
 
-    local function handleTaskAction(action, task)
-        local db = ModData.getOrCreate(MODDATA_KEY)
-    
-        if action == "CreateTask" then
-            task.id = generateUUID()
-            db[task.id] = task
-            
-        elseif action == "UpdateTask" and task.id then
-            db[task.id] = task
+local function handleTaskAction(action, task)
+    local db = ModData.getOrCreate(MODDATA_KEY)
 
-        elseif action == "DeleteTask" and task.id then
-            db[task.id] = nil
+    if action == "CreateTask" then
+        task.id = generateUUID()
+        db[task.id] = task
 
-        elseif action == "DeleteAllTasks" then
-            table.wipe(db)
-        
-        elseif action == "FetchAllTasks" then
-            db = task
+    elseif action == "UpdateTask" and task.id then
+        db[task.id] = task
 
-        elseif action == "AssignTasks" then
-            db = task
-        end
-        
+    elseif action == "DeleteTask" and task.id then
+        db[task.id] = nil
+
+    elseif action == "DeleteAllTasks" then
+        table.wipe(db)
+
+    elseif action == "FetchAllTasks" then
         return db
-    end
-    
-    function TaskBoard_Core.fetchAllTasks() 
-        -- server
-        if command == "FetchAllTasks" then
-            kb_DataManager.tasks = args.tasks
-        end
-    
+
+    elseif action == "AssignTasks" then
+        db = task
     end
 
-    function TaskBoard_Core.reloadAll(tasks)
-        if isClient() then
-            sendClientCommand(MODDATA_KEY, "ReloadAllTables", task)
-        elseif isServer() then
-            handleTaskAction("AssignTasks", task)
-            reloadAllTablesInClient(db)
-        else
-            handleTaskAction("AssignTasks", task)
-            reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
-        end
+    return db
+end
 
-        -- server
-        if command == "ReloadAllTables" then
-            kb_DataManager.tasks = args.tasks
-            reloadAllTablesInClient(kb_DataManager.tasks)
-        end
+function TaskBoard_Core.reloadAllTables(player)
+    if isClient() then
+        sendClientCommand(MODDATA_KEY, "ReloadAllTables", {})
+    elseif isServer() then
+        local db = handleTaskAction("FetchAllTasks")
+        sendServerCommand(player, MODDATA_KEY, "ReloadAllTables", { tasks = db })
+    else
+        reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
     end
-    
-    function TaskBoard_Core.create(task)
-        print("[TaskBoard_Core.create] Creating task with data:")
-    
-        if isClient() then
-            print("[TaskBoard_Core.create] Detected isClient() - sending ClientCommand")
-            sendClientCommand(MODDATA_KEY, "CreateTask", task)
-        elseif isServer() then
-            print("[TaskBoard_Core.create] Detected isServer() - handling and transmitting ModData")
-            handleTaskAction("CreateTask", task)
-            ModData.transmit(MODDATA_KEY)
-        else
-            print("[TaskBoard_Core.create] Detected singleplayer - handling and reloading tables")
-            handleTaskAction("CreateTask", task)
-            reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
-        end
-    end
-    
-    function TaskBoard_Core.update(task)
-        if not task.id then return end
+end
 
-        if isClient() then
-            sendClientCommand(MODDATA_KEY, "UpdateTask", task)
-        elseif isServer() then
-            handleTaskAction("UpdateTask", task)
-            ModData.transmit(MODDATA_KEY)
-        else
-            handleTaskAction("UpdateTask", task)
-            reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
-        end
+function TaskBoard_Core.create(task)
+    if isClient() then
+        sendClientCommand(MODDATA_KEY, "CreateTask", task)
+    elseif isServer() then
+        handleTaskAction("CreateTask", task)
+        ModData.transmit(MODDATA_KEY)
+    else
+        handleTaskAction("CreateTask", task)
+        reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
+    end
+end
+
+function TaskBoard_Core.update(task)
+    if not task.id then return end
+
+    if isClient() then
+        sendClientCommand(MODDATA_KEY, "UpdateTask", task)
+    elseif isServer() then
+        handleTaskAction("UpdateTask", task)
+        ModData.transmit(MODDATA_KEY)
+    else
+        handleTaskAction("UpdateTask", task)
+        reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
+    end
+end
+
+function TaskBoard_Core.delete(task)
+    if isClient() then
+        sendClientCommand(MODDATA_KEY, "DeleteTask", task)
+    elseif isServer() then
+        handleTaskAction("DeleteTask", task)
+        ModData.transmit(MODDATA_KEY)
+    else
+        handleTaskAction("DeleteTask", task)
+        reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
+    end
+end
+
+function generateUUID()
+    local db = ModData.getOrCreate(MODDATA_KEY)
+
+    local function padWithZeros(num)
+        return string.format("%010d", num)
     end
 
-    function TaskBoard_Core.delete(task)
-        if isClient() then
-            sendClientCommand(MODDATA_KEY, "DeleteTask", task)
-        elseif isServer() then
-            handleTaskAction("DeleteTask", task)
-            ModData.transmit(MODDATA_KEY)
-        else
-            handleTaskAction("DeleteTask", task)
-            reloadAllTablesInClient(ModData.getOrCreate(MODDATA_KEY))
-        end
+    for i = 1, 5 do
+        local id = padWithZeros(ZombRand(0, 1000000000))
+        if not db[id] then return id end
     end
-    
-    function generateUUID()
-        local db = ModData.getOrCreate(MODDATA_KEY)
-    
-        local function padWithZeros(num)
-            return string.format("%010d", num) -- Ensures the number is 10 digits long with leading zeros
-        end
-    
-        for i = 1, 5 do
-            local id = padWithZeros(ZombRand(0, 1000000000)) -- Generate a number and pad it
-            if not db[id] then return id end
-        end
-    
-        return padWithZeros(ZombRand(0, 1000000000)) -- Fallback in case of collision
-    end
+
+    return padWithZeros(ZombRand(0, 1000000000))
+end
